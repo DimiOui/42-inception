8191c0eba8ac483ff2713c5274fbc7fa9dd0f5e6
DimiOui <omygoditsdimi@gmail.com>
2022-11-09 17:13:10 +0100

    Inception

--- srcs/requirements/mariadb/tools/mariadb_init.sh
+++ srcs/requirements/mariadb/tools/mariadb_init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ ! -d "/var/lib/mysql/${MYSQL_PATH}" ]; then
-    #Installing Mariadb database
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Installing Mariadb Database"
     mysql_install_db > /dev/null
     temp=`mktemp`
     cat << EOF > $temp
 
+FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DB};
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';
@@ -13,7 +14,7 @@ CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* to '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
-    #Intitializing Mariadb database
+    echo "Intitializing Mariadb database"
     mysqld --bootstrap < $temp && rm $temp
 else
     echo "Mariadb Database already installed"
--- srcs/requirements/wordpress/tools/create_wp.sh
+++ srcs/requirements/wordpress/tools/create_wp.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-#Configuring php-fpm
+echo "Configuring php-fpm"
 echo "listen = 9000 " >> /etc/php8/php-fpm.d/www.conf
-#Checking for mariadb
+sed -i "s/;error_log/error_log/" /etc/php8/php-fpm.conf
+ln -sf /dev/stderr /var/log/php8/error.log
+echo "Checking for mariadb"
 while ! mariadb -h${MYSQL_HOSTNAME} -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DB} > /dev/null;
 do
-
+	echo "waiting for mariadb"
 	sleep 2
-
 done
 
 if [ ! -f "index.php" ]
 then
-	#Installing Wordpress
+	echo "Installing Wordpress"
 	wp core download	--path="/var/www/html"
 	wp config create	--dbname=${MYSQL_DB} \
 						--dbuser=${MYSQL_USER} \
@@ -21,9 +22,7 @@ then
 						--allow-root
 	if [ ! -f "wp-config.php" ]
 	then
-
 		return 1
-
 	fi
 	wp core install		--url="dpaccagn.42.fr" \
 						--title=${WP_TITLE} \
@@ -39,15 +38,14 @@ then
 
 	wp theme install "twentyseventeen" --activate --allow-root
 
-	#wp search-replace 'http://dpaccagn.42.fr' 'https://dpaccagn.42.fr' > /dev/null
+	wp search-replace 'http://dpaccagn.42.fr' 'https://dpaccagn.42.fr' > /dev/null
 
 	wp post generate	--count=1 \
 						--post_author="dpaccagn" \
-						--post_title="Peanut Butter" \
-						--post_content="Miam miam le peanut butter" \
+						--post_title="I N C E P T I O N" \
+						--post_content="Get inceptioned" \
 						--post_status=publish \
 						--allow-root
 
 fi
-
 echo "Running php-fpm" && php-fpm8 -FR
